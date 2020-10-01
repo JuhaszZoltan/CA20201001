d49587c3134497c84ef6270eff6a157d497d3334
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2020-10-01 12:27:17 +0200

    Add project files.

---
+++ CA20201001/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA20201001
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main()
+        {
+            int[] t = new int[20];
+            float k = 10F;
+            float v = 15F;
+            for (int i = 0; i < t.Length; i++)
+            {
+                //határértékek:
+                //Console.WriteLine($"{(int)k} --- {(int)v}");
+                t[i] =  rnd.Next((int)k, (int)v);
+                k += 4.5F;
+                v += 4.5F;
+                Console.Write($"{t[i]} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
